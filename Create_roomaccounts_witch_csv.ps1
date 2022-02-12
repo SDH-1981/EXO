9efa17bcd8581d933073ae3939a97487d23085df
$tef@nH <96553047+SDH-1981@users.noreply.github.com>
2022-02-12 18:08:27 +0100

    Add files via upload

---
+++ Create_roomaccounts_witch_csv.ps1
@@ -0,0 +1,5 @@
+﻿#Script creates roombailboxes from csv with location and capacity
+#StefanH
+###################################################################################################
+Import-csv .\rooms.csv | 
+foreach{ New-Mailbox -Name $_.roomname -Room -PrimarySmtpAddress $_.emailaddress -Office $_.Location -ResourceCapacity $_.Capacity }
